c14ac4f2d757bdb909b4c20af56ed6b1bda7ba5c
Ronny Ac[acio <ronnyacacio27@gmail.com>
2020-02-08 18:42:33 -0300

    Finish

---
+++ nubank/src/components/Header/styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.View`
+  align-items: center;
+  padding: 40px 0 30px;
+  margin-top: 10px;
+`;
+
+export const Top = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 5px;
+`;
+
+export const Logo = styled.Image``;
+
+export const Title = styled.Text`
+  font-size: 18px;
+  color: #fff;
+  font-weight: bold;
+  margin-left: 8px;
+`;
---
+++ nubank/src/components/Main/index.js
@@ -0,0 +1,109 @@
+import React from "react";
+import { StatusBar, Animated } from "react-native";
+
+import { PanGestureHandler, State } from "react-native-gesture-handler";
+import Icon from "react-native-vector-icons/MaterialIcons";
+
+import Header from "../Header";
+import Tabs from "../Tabs";
+import Menu from "../Menu";
+
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Title,
+  Description,
+  Annotation
+} from "./styles";
+
+export default function Main() {
+  let offset = 0;
+  const translateY = new Animated.Value(0);
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: { translationY: translateY }
+      }
+    ],
+    { useNativeDriver: true }
+  );
+
+  function onHandleStateChanged(event) {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      let opened = false;
+      const { translationY } = event.nativeEvent;
+
+      offset += translationY;
+
+      translateY.setOffset(offset);
+      translateY.setValue(0);
+
+      if (translationY >= 100) {
+        opened = true;
+      } else {
+        translateY.setValue(offset);
+        translateY.setOffset(0);
+        offset = 0;
+      }
+      Animated.timing(translateY, {
+        toValue: opened ? 380 : 0,
+        duration: 200,
+        useNativeDriver: true
+      }).start(() => {
+        offset = opened ? 380 : 0;
+        translateY.setOffset(offset);
+        translateY.setValue(0);
+      });
+    }
+  }
+
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#8B10AE" />
+      <Container>
+        <Header />
+        <Content>
+          <Menu translateY={translateY} />
+          <PanGestureHandler
+            onGestureEvent={animatedEvent}
+            onHandlerStateChange={onHandleStateChanged}
+          >
+            <Card
+              style={{
+                transform: [
+                  {
+                    translateY: translateY.interpolate({
+                      inputRange: [-200, 0, 380],
+                      outputRange: [-25, 0, 380],
+                      extrapolate: "clamp"
+                    })
+                  }
+                ]
+              }}
+            >
+              <CardHeader>
+                <Icon name="attach-money" size={28} color="#666" />
+                <Icon name="visibility-off" size={28} color="#666" />
+              </CardHeader>
+              <CardContent>
+                <Title>Saldo disponível</Title>
+                <Description>R$ 102.611,65</Description>
+              </CardContent>
+              <CardFooter>
+                <Annotation>
+                  Transferência de R$ 20,00 recebida de Douglas Santos hoje às
+                  20:00h
+                </Annotation>
+              </CardFooter>
+            </Card>
+          </PanGestureHandler>
+        </Content>
+        <Tabs translateY={translateY} />
+      </Container>
+    </>
+  );
+}
